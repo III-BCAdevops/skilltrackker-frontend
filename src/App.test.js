95f365cc633e9267154f450e9c91a329ff32f98e
SELVARATHI V <23suca53@tcarts.in>
2026-03-23 14:54:52 +0530

    Delete src/App.test.js

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders skill tracker title', () => {
-  render(<App />);
-  const heading = screen.getByRole('heading', { name: /skill tracker/i });
-  expect(heading).toBeInTheDocument();
-});
